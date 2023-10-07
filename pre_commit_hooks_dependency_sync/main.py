4cc45cb5964104fce169501f3de0e587780fda86
Will Garside <worgarside@gmail.com>
2023-10-07 11:50:25 +0100

    Bugfixes

--- pre_commit_hooks_dependency_sync/main.py
+++ pre_commit_hooks_dependency_sync/main.py
@@ -3,19 +3,23 @@
 
 from __future__ import annotations
 
+import re
 from pathlib import Path
 
 from pkg_resources import parse_requirements
 from tomli import load
-from yaml import dump, safe_load
+from yaml import safe_load
 
 REPO_PATH = Path().cwd()
 
 POETRY_LOCKFILE = REPO_PATH / "poetry.lock"
 PCH_CONFIG = REPO_PATH / ".pre-commit-config.yaml"
 
+ADD_DEP_PREFIX = r"(\s+-\s)"
 
-def main() -> None:
+
+def get_replacements() -> dict[str, str]:
+    """Get a dictionary of dependencies to replace in the pre-commit config."""
     with POETRY_LOCKFILE.open("rb") as poetry_lockfile:
         packages = load(poetry_lockfile).get("package", [])
 
@@ -24,32 +28,42 @@ def main() -> None:
     with PCH_CONFIG.open("r") as pch_config:
         config = safe_load(pch_config)
 
+    replacements = {}
+
     for repo in config["repos"]:
         for hook in repo.get("hooks", []):
-            updates = {}
-
             for req in parse_requirements(hook.get("additional_dependencies", [])):
                 if (
                     target_version := installed.get(req.key)
                 ) and req.specifier != f"=={target_version}":
-                    updates[req.key] = f"=={target_version}"
+                    replacements[
+                        f"{req.key}{req.specifier}"
+                    ] = f"{req.key}=={target_version}"
 
-            if updates:
-                hook["additional_dependencies"] = sorted(
-                    [
-                        x
-                        for x in hook["additional_dependencies"]
-                        if next(parse_requirements(x)).key not in updates
-                    ]
-                    + [f"{req}{version}" for req, version in updates.items()],
-                    key=lambda x: next(parse_requirements(x)).name,
-                )
+    return replacements
 
-    raw_yaml = dump(config, indent=2, sort_keys=False)
 
-    pretty_yaml = "---\n" + raw_yaml.replace("\n- ", "\n\n- ")
+def main() -> None:
+    """Update the pre-commit config with the latest versions of dependencies."""
+    with PCH_CONFIG.open("r") as f:
+        lines = f.readlines()
 
-    PCH_CONFIG.write_text(pretty_yaml)
+    updated_lines = []
+    apply_updates = False
+
+    for line in lines:
+        updated_line = line
+        for key, value in get_replacements().items():
+            updated_line = re.sub(
+                f"^{ADD_DEP_PREFIX}{re.escape(key)}$", f"\\1{value}", updated_line
+            )
+
+        apply_updates |= updated_line != line
+        updated_lines.append(updated_line)
+
+    if apply_updates:
+        with PCH_CONFIG.open("w") as f:
+            f.writelines(updated_lines)
 
 
 if __name__ == "__main__":
